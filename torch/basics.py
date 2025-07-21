bfb3eebb40ede528214948cd7b585b3028761daf
BalaMahalakshmi <sankarisri8578@gmail.com>
2025-07-21 20:57:11 +0530

    updating basics file

--- torch/basics.py
+++ torch/basics.py
@@ -92,3 +92,90 @@ t = torch.tensor([5,8,6])
 # print(t-30)
 # print(t)
 
+# MATRIX multiplication
+
+# print(t*t)
+# print(f"equals:{t*t}")
+
+# print(torch.matmul(t,t))
+
+TimeoutError
+value = 0
+for i in range(len(t)):
+    value+= t[i] * 2
+# print(value)
+
+# print(t @ t)
+
+TimeoutError
+# print(torch.matmul(t,t))
+
+a = torch.tensor([[2,4],[6,8],[3,5]])
+b = torch.tensor([[7,12],[8,16],[6,3]])
+# print(b.T)
+# print(torch.matmul(a, b.T))
+# print(f"original shape:a={a.shape} , b={b.shape}")
+# print(f"new shape: a={a.shape}, b.T={b.T.shape}")
+# print(f"multiply:{a.shape}@{b.T.shape}")
+# print("op:\n")
+op = torch.matmul(a, b.T)
+# print(op)
+# print(f"op shape:{op.shape}")
+
+
+x = torch.arange(0,100,10)
+# print(x.min())
+# print(x.max())
+# print(x.type(torch.float32).mean())
+# print(x.sum())
+# print(x)
+
+# print(x.argmin())
+# print(x.argmax())
+# print(x[0])
+# print(x[9]) 
+
+#reshape
+
+x = torch.arange(1.,10.)
+# print(x.shape)
+a = x.reshape(9,1)
+# print(a.shape)
+
+#view
+
+z = x.view(1,9)
+# print(z)
+
+z[:,0] = 8
+# print(z)
+
+#stack
+xs = torch.stack([x,x,x], dim=1)
+# print(xs)
+
+#squeeze
+# print(a.squeeze().shape)
+
+#unsqeeze
+# print(a.unsqueeze(dim=0))
+
+#permute
+# print(a.permute(1,0))
+
+x = torch.rand(size=(224,224,3))
+xp = x.permute(2,0,1)
+# print(xp)
+# print(xp.shape)
+# print(x[:,:,0])
+
+
+#indexing
+a = torch.arange(1,10).reshape(1,3,3)
+# print(a)
+# print(a[0])
+# print(a[0][0])
+# print(a[0][2])
+# print(a[:,0])
+# print(a[:,:,2])
+print(a[:,1,1])
