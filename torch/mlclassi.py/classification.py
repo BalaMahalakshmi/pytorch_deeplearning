04f4c6c35021fd6029b63ec9c9cc0e7696296435
BalaMahalakshmi <sankarisri8578@gmail.com>
2025-07-23 20:19:52 +0530

    downloading new file using requests library

--- torch/mlclassi.py/classification.py
+++ torch/mlclassi.py/classification.py
@@ -73,7 +73,109 @@ untrained_pred = m0(x.to(device))
 
 #setup the loss function
 # lf  = nn.BCELoss() #through sigmoid fun.
-lf = nn.BCEWithLogitsLoss
+lf = nn.BCEWithLogitsLoss()
+optim = torch.optim.SGD(params=m0.parameters(), lr=0.1)
+# print(optim)
+
+#calculate accuracy
+
+def accuracy_fn(y_true, pred):
+    correct = torch.eq(y_true, pred).sum().item()
+    acc = (correct/len(pred))*100
+    # print(acc)
+
+m0.eval()
+with torch.inference_mode():
+    y_logits = m0(x.to(device))[:5]
+# print(y_logits)
+
+# print(y)[:5]
+
+#use sigmoid function
+y_pred_probs = torch.sigmoid(y_logits)
+# print(y_pred_probs)
+
+#find the predicted labels
+preds =torch.round(y_pred_probs)
+#in full
+pred_labels = torch.round(torch.sigmoid(m0(x.to(device))[:5]))
+#check for equality
+# print(torch.eq(preds.squeeze(), pred_labels.squeeze()))
+#get rid of extra dim.
+# print(preds.squeeze())
+
+#building training and test model
+
+torch.manual_seed(42)
+torch.cuda.manual_seed(42)
+
+#set epochs
+epochs=100
+
+#put data to target device
+x1,y1 = x1.to(device), y1.to(device)
+x2,y2 = x2.to(device), y2.to(device)
+
+#build training and evaluation loop
+for epoch in range(epochs):
+    #training 
+    m0.train()
+
+    #forward pass
+    y_logits = m0(x1).squeeze()
+    preds = torch.round(torch.sigmoid(y_logits))
+    # print("prediction:",preds)
+
+    #calculate loss/accuracy
+    loss = lf(torch.sigmoid(y_logits), y1 )
+    # print(lf, loss)
+    acc = accuracy_fn(y_true=y1, pred=preds)
+    # print("accuracy:", acc)
+
+#oprimizer zero grad
+optim.zero_grad()
+
+loss.backward()
+optim.step()
+
+#testing
+m0.eval()
+with torch.inference_mode():
+    tl = m0(x2).squeeze()
+    tp = torch.round(torch.sigmoid(tl))
+    # print("test_loss:",tl)
+    # print("test_pred:",tp)
+
+    test_loss = lf(tl, y2)
+    # print("loss:",test_loss)
+    test_acc = accuracy_fn(y_true=y2, pred = tp)
+    # print("accuracy:", test_acc)
+
+if epoch % 10 == 0:
+    print(f"epoch: {epoch} | loss:{lf:.5}, accuracy:{acc:.2} % | test loss:{test_loss:.5}, test acc:{test_acc:2f} %")
+
+
+import requests
+from pathlib import Path
+
+if Path ("helper_functions.py").is_file():
+    print("helper_functions.py already exits, skipping download")
+else:
+    print("download helper_functions.py")
+    with open("helper_functions.py", "wb") as f:
+        f.write(requests.content)
+
+from helper_functions import plot_predictions, plot_decision_boundary
+
+plt.figure(figsize=(10,8))
+plt.subplot(1,2,1)
+plt.title("Train")
+plot_decision_boundary(m0, x1, y1)
+plt.subplot(1,2,2)
+plt.title("Test")
+plot_decision_boundary(m0, x2, y2)
+plt.show()
+
 
 
 
