73434a8d75383aa989dd7ac415b96e934f9cc163
BalaMahalakshmi <sankarisri8578@gmail.com>
2025-08-08 22:34:56 +0530

    modifying cnn

--- torch/cnn.py
+++ torch/cnn.py
@@ -58,12 +58,20 @@ class FashinonMNISTModelv2(nn.Module):
         # print(x.shape)
         x = self.conv_block_2(x)
         return x
-
-
+    
 train_data = datasets.FashionMNIST(root="data", train=True, download=True, 
                                    transform=torchvision.transforms.ToTensor(), target_transform=None)
 test_data = datasets.FashionMNIST(root="data", train=False, download=True, 
                                    transform=ToTensor(), target_transform=None)
+batchsize = 32
+train_dataloader = DataLoader(dataset = train_data, batch_size= batchsize, shuffle=True)
+test_dataloader = DataLoader(dataset=test_data, batch_size=batchsize, shuffle=True)
+    
+train_features_batch , train_labels_batch = next(iter(train_dataloader))
+rand_idx = torch.randint(0, len(train_features_batch), size=[1]).item()
+img, label = train_features_batch[rand_idx], train_labels_batch[rand_idx]
+
+
 class_name = train_data.classes
 torch.manual_seed(42)
 model_2 = FashinonMNISTModelv2(input_shape=1,
@@ -88,3 +96,32 @@ con_output =conv_layer(test_image)
 # print(con_output)
 # print(test_image.unsqueeze(0).shape)
 
+# print(test_image.shape)
+# print(f"Test image original shape: {test_image.shape} ")
+# print(f"Test image with unsqueezed dimension: {test_image.unsqueeze(0).shape}")
+max_pool_layer = nn.MaxPool2d(kernel_size=2)
+test_image_through_conv = conv_layer(test_image.unsqueeze(dim=0))
+# print(f"shape after going through conv_layer(): {test_image_through_conv.shape}")
+test_image_through_conv_and_max_pool = max_pool_layer(test_image_through_conv)
+# print(f"shape after going through conv_layer() and max_pool_layer(): {test_image_through_conv}")
+
+torch.manual_seed(42)
+# create a random tensor
+random_tensor = torch.randn(size=(1,1,2,2))
+# print(f"\n Random tensor: \n{random_tensor}")
+# print(f"Random tensor shape: {random_tensor.shape}")
+
+# create a max pool layer 
+max_pool_tensor = max_pool_layer(random_tensor)
+
+# pass the random tensor through max pool layer
+# print(f"\n Max pool tensor:\n {max_pool_tensor}")
+# print(f"max pool tensor shape: {max_pool_tensor.shape}")
+# print(random_tensor)
+
+torch.manual_seed(42)
+m0 = FashinonMNISTModelv2(input_shape=1,
+                         hidden_units=10,
+                         output_shape=len(class_name)).to("cpu")
+plt.imshow(img.squeeze(), cmap='grey')
+# plt.show()
